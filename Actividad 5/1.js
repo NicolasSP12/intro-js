47644ef37300b14dc15a040745d35ebcd455dc46
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-02-01 22:33:56 -0300

    Actividad 5

---
+++ Actividad 5/1.js
@@ -0,0 +1,6 @@
+var arreglo = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+
+for(var i = 0; i < arreglo.length; i++) { 
+    // console.log(arreglo[i]) Para que se iteren todos los numeros
+    if(arreglo[i]>3){console.log(arreglo[i])} //Para que solo se iteren los numeros mayores a 3
+}
