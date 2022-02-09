f29eb876a7e57f342ca5fa879bb6db9f2c100365
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-02-09 00:07:12 -0300

    Actividad 9 (completada)

---
+++ Actividad 9/5.js
@@ -0,0 +1,10 @@
+//Escriba un programa para modificar el valor de la segunda propiedad del siguiente objeto:
+var student = {
+    name: "David Rayy",
+    age: 34,
+    job: "engineer",
+    hobbies: ['Sky', 'football', 'walking my dog']
+};
+//La nueva edad debe ser 35.
+student.age = 35
+console.log(student.age)
---
+++ Actividad 9/7.js
@@ -0,0 +1,14 @@
+//Crea un código para llenar un objecto vacio:
+ const objeto = {}
+
+// Con las siguiente propiedades:
+//color
+//tamaño
+//peso
+//cantidad
+
+objeto.color = "azul"
+objeto.tamaño = "grande"
+objeto.peso = 15
+objeto.cantidad = 32
+console.log(objeto)
